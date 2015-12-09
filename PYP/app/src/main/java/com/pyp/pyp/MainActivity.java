327fc35207575f62e1004638a657a0feb267ba5d
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-12-09 16:23:24 -0500

    GSR and Heart Rate are now loggable

--- PYP/app/src/main/java/com/pyp/pyp/MainActivity.java
+++ PYP/app/src/main/java/com/pyp/pyp/MainActivity.java
@@ -16,6 +16,12 @@ import com.microsoft.band.BandException;
 import com.microsoft.band.BandInfo;
 import com.microsoft.band.BandPendingResult;
 import com.microsoft.band.ConnectionState;
+import com.microsoft.band.UserConsent;
+import com.microsoft.band.sensors.BandGsrEvent;
+import com.microsoft.band.sensors.BandGsrEventListener;
+import com.microsoft.band.sensors.BandHeartRateEvent;
+import com.microsoft.band.sensors.BandHeartRateEventListener;
+import com.microsoft.band.sensors.HeartRateConsentListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
     BandClient bandClient;
 
     TextView feedback;
-    Button attach;
+    Button attach, read_button;
+
+    Boolean attached;
 
     BandPendingResult<ConnectionState> pendingResult;
 
@@ -32,11 +40,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        feedback = (TextView) findViewById(R.id.feedback);
+
+        attached = false;
+
         attach = (Button) findViewById(R.id.band_attach);
         attach.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 attachBand();
+                attached = true;
+            }
+        });
+
+        read_button = (Button) findViewById(R.id.read_button);
+        read_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (attached) {
+                    feedback.setText("Reading data..");
+                    readData();
+                } else {
+                    feedback.setText("Band not attached yet");
+                }
             }
         });
     }
@@ -45,12 +71,82 @@ public class MainActivity extends AppCompatActivity {
         pairedBands = BandClientManager.getInstance().getPairedBands();
         bandClient = BandClientManager.getInstance().create(this, pairedBands[0]);
 
-        feedback = (TextView) findViewById(R.id.feedback);
         feedback.setText("Band Attached");
 
         //Need to place this in an Async Task
         new ConnectBandTask().execute();
         return true;
+    }
+
+    public void readData() {
+        final BandGsrEventListener gsrListener = new BandGsrEventListener() {
+            @Override
+            public void onBandGsrChanged(BandGsrEvent bandGsrEvent) {
+                //feedback.setText("GSR: " + bandGsrEvent.getResistance());
+                Log.d("Thing", "GSR: " + bandGsrEvent.getResistance());
+            }
+        };
+
+        final BandHeartRateEventListener heartRateListener = new BandHeartRateEventListener() {
+            @Override
+            public void onBandHeartRateChanged(BandHeartRateEvent bandHeartRateEvent) {
+                //feedback.setText("Heart Rate: " + bandHeartRateEvent.getHeartRate());
+                Log.d("Thing", "Heart Rate: " + bandHeartRateEvent.getHeartRate());
+            }
+
+        };
+
+        Log.d("Thing", "Checking consent");
+
+//        try {
+        // register the listener
+            if(bandClient.getSensorManager().getCurrentHeartRateConsent() !=
+                    UserConsent.GRANTED) {
+                // user hasn’t consented, request consent
+                // the calling class is an Activity and implements
+                // HeartRateConsentListener
+                Log.d("Thing", "Not consented");
+                bandClient.getSensorManager().requestHeartRateConsent(this, new HeartRateConsentListener() {
+                    @Override
+                    public void userAccepted(boolean b) {
+                        startHeartRate(heartRateListener);
+                        startGSR(gsrListener);
+                    }
+                });
+            }
+        else {
+                Log.d("Thing", "Consented");
+                startHeartRate(heartRateListener);
+                startGSR(gsrListener);
+            }
+
+
+//            bandClient.getSensorManager().registerGsrEventListener(
+//                    gsrListener);
+//        } catch(BandException ex) {
+//        // handle BandException
+//            Log.d("Thing", "Band exception occured: " + ex);
+//        }
+
+    }
+
+    public void startHeartRate(BandHeartRateEventListener listener)  {
+        try {
+            bandClient.getSensorManager().registerHeartRateEventListener(listener);
+        }
+        catch (BandException e) {
+            Log.d("Thing", "Heart rate band exception: " + e);
+        }
+
+    }
+
+    public void startGSR (BandGsrEventListener listener) {
+        try {
+            bandClient.getSensorManager().registerGsrEventListener(listener);
+        }
+        catch (BandException e) {
+            Log.d("Thing", "GSR band exception: " + e);
+        }
     }
 
 
