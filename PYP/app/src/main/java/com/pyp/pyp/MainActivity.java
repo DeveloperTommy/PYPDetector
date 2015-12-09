c8a15a75d6fff714307febffa05f470e98201672
DeveloperTommy <its.tommy.nguyen@gmail.com>
2015-12-09 15:43:36 -0500

    Connection with band successful

--- PYP/app/src/main/java/com/pyp/pyp/MainActivity.java
+++ PYP/app/src/main/java/com/pyp/pyp/MainActivity.java
@@ -1,16 +1,56 @@
 package com.pyp.pyp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.microsoft.band.BandClient;
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.BandPendingResult;
+import com.microsoft.band.ConnectionState;
 
 public class MainActivity extends AppCompatActivity {
+
+    BandInfo[] pairedBands;
+    BandClient bandClient;
+
+    TextView feedback;
+    Button attach;
+
+    BandPendingResult<ConnectionState> pendingResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        attach = (Button) findViewById(R.id.band_attach);
+        attach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attachBand();
+            }
+        });
+    }
+
+    public boolean attachBand() {
+        pairedBands = BandClientManager.getInstance().getPairedBands();
+        bandClient = BandClientManager.getInstance().create(this, pairedBands[0]);
+
+        feedback = (TextView) findViewById(R.id.feedback);
+        feedback.setText("Band Attached");
+
+        //Need to place this in an Async Task
+        new ConnectBandTask().execute();
+        return true;
     }
 
 
@@ -35,4 +75,35 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class ConnectBandTask extends AsyncTask<Void, Void, Void> {
+        protected Void doInBackground(Void... nothing) {
+            pendingResult = bandClient.connect();
+            try {
+                ConnectionState state = pendingResult.await();
+                if(state == ConnectionState.CONNECTED) {
+                    // do work on success
+                    Log.d("Thing", "Success");
+                } else {
+                    // do work on failure
+                    Log.d("Thing", "Unsuccessful");
+                }
+            } catch(InterruptedException ex) {
+                // handle InterruptedException
+            } catch(BandException ex) {
+                // handle BandException
+            }
+            return null;
+        }
+
+        protected void onProgressUpdate() {
+
+        }
+
+        protected void onPostExecute() {
+
+        }
+    }
+
+
 }
